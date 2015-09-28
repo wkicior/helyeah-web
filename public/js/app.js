cf60c1d353613c15af46b4eacc0dc20354e99d09
Wojciech Kicior <diso.junk+github@gmail.com>
2015-09-28 21:08:55 +0200

    Registration view and controller

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,9 @@ helyeah.config(['$routeProvider',function($routeProvider) {
     $routeProvider.when('/notificationPlans', {
 	templateUrl: 'templates/notifications/notificationPlans.html',
 	controller: 'NotificationPlansController'
+    }).when('/register', {
+	templateUrl: 'templates/users/registration.html',
+	controller: 'RegistrationController'
     }).otherwise({
 	redirectTo: '/notificationPlans'
     });			
---
+++ public/js/users/registrationController.js
@@ -0,0 +1,3 @@
+helyeah.controller('RegistrationController', ['$scope', '$http', function($scope, $http) {
+    $scope.user = {};
+}
